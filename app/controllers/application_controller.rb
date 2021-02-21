0448b6ac5c6dbcfe0b2dc6ba7303d82fd8c6186c
houtaru95 <ottytai@gmail.com>
2021-02-21 18:42:18 +0900

    first commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+class ApplicationController < ActionController::Base
+  before_action :configure_permitted_patameters, if: :devise_controller?
+
+
+  private
+
+  def configure_permitted_patameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :profile, :occupation, :position])
+  end
+
+
+end
